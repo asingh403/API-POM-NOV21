33603961d847f82d04987f6f6534c8e205c83983
asingh4305 <asingh.it@hotmail.com>
2021-10-27 08:28:03 +0530

    post call added

--- src/main/java/com/qa/api/gorest/restclient/RestClient.java
+++ src/main/java/com/qa/api/gorest/restclient/RestClient.java
@@ -2,6 +2,8 @@ package com.qa.api.gorest.restclient;
 
 import java.util.Map;
 
+import com.qa.api.gorest.util.TestUtil;
+
 import io.restassured.*;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -18,21 +20,80 @@ import io.restassured.specification.RequestSpecification;
 public class RestClient {
 
 	// HTTP Method: GET, POST, PUT, DELETE
+	
+	/**
+	 * This method is used to call GET api's
+	 * @param contentType
+	 * @param baseURI
+	 * @param basePath
+	 * @param token
+	 * @param paramsMap
+	 * @param log
+	 * @return This method is returning Response from the GET call.
+	 */
 
 	public static Response doGet(String contentType, String baseURI, String basePath, String token,
 			Map<String, String> paramsMap, boolean log) {
 		
-		setBaseUri(baseURI);
-		RequestSpecification request = createRequest(contentType, token, paramsMap, log);
-		return getResponse("GET", request, basePath);
+		if(setBaseUri(baseURI)) {
+			RequestSpecification request = createRequest(contentType, token, paramsMap, log);
+			return getResponse("GET", request, basePath);			
+		}
+		return null;
+	}
+	
+	/**
+	 * This method is used to call POST api's
+	 * @param contentType
+	 * @param baseURI
+	 * @param basePath
+	 * @param token
+	 * @param paramsMap
+	 * @param log
+	 * @param obj
+	 * @return This method is returning Response from the POST call.
+	 */
+	public static Response doPost(String contentType, String baseURI, String basePath, String token,
+			Map<String, String> paramsMap, boolean log, Object obj) {
+		
+		if(setBaseUri(baseURI)) {
+			RequestSpecification request = createRequest(contentType, token, paramsMap, log);
+			addRequestPayload(request, obj);
+			return getResponse("POST", request, basePath);			
+		}
+		return null;
+	}
+	
+	/**
+	 * Creating add payload for the doPost method
+	 * @param request
+	 * @param obj
+	 */
+	
+	public static void addRequestPayload(RequestSpecification request, Object obj) {
+		String jsonPayload = TestUtil.getSerializedJSON(obj);
+		request.body(jsonPayload);
 		
 	}
 	
-	public static void setBaseUri(String baseURI) {
-		RestAssured.baseURI = baseURI; 
+	private static boolean setBaseUri(String baseURI) {
+		
+		if(baseURI==null || baseURI.isEmpty()) {
+			System.out.println("Please pass the baseURI....");
+			return false;
+		}
+		
+		try {
+		RestAssured.baseURI = baseURI;
+		return true;
+
+		} catch(Exception e) {
+			System.out.println("Some exception got occured while assigining the baseURI with Rest Assured. . .");
+			return false;
+		}
 	}
 	
-	public static RequestSpecification createRequest(String contentType, String token, Map<String, String> paramMap, boolean log) {
+	private static RequestSpecification createRequest(String contentType, String token, Map<String, String> paramMap, boolean log) {
 		
 		RequestSpecification request;
 		
@@ -68,12 +129,12 @@ public class RestClient {
 	}
 	
 	
-	public static Response getResponse(String httpMethod, RequestSpecification request, String basePath) {
+	private static Response getResponse(String httpMethod, RequestSpecification request, String basePath) {
 		
 		return executeAPI(httpMethod, request, basePath);
 	}
 	
-	public static Response executeAPI(String httpMethod, RequestSpecification request, String basePath) {
+	private static Response executeAPI(String httpMethod, RequestSpecification request, String basePath) {
 		
 		Response response = null;
 		
--- src/test/java/com/qa/api/gorest/tests/CreateUserTest.java
+++ src/test/java/com/qa/api/gorest/tests/CreateUserTest.java
@@ -1,5 +1,29 @@
 package com.qa.api.gorest.tests;
 
+import org.testng.annotations.Test;
+
+import com.qa.api.gorest.restclient.RestClient;
+import com.qa.gorest.pojo.User;
+
+import io.restassured.response.Response;
+
 public class CreateUserTest {
+	
+	String baseURI = "https://gorest.co.in";
+	String basePath = "/public/v1/users";
+	String token = "62aa1f2dc00580c92239cdfcb96da7e6cae2b0022e2ea6be603dda8fd48b03c6";
+	
+	@Test
+	public void createUserAPIPOSTTest() {
+		
+		User user = new User("Aditya", "adi0012@hotmail.com", "male", "25-aug-1998", "active");
+		Response response = RestClient.doPost("JSON", baseURI, basePath, token, null, true, user);
+		
+		System.out.println(response.asPrettyString());
+		System.out.println(response.statusCode());
+		
+	}
+	
+	
 
 }
