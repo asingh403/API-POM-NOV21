da67f1bc4d2a441864cf0e3773a793639c10b365
asingh4305 <asingh.it@hotmail.com>
2021-11-09 10:12:53 +0530

    token utility added

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # API-POM-NOV21
 I will develop as E2E framework along with Extent, Allure and PDF report. Which will include CI/CD Pipline also including Jenkins as well.
+<<<<<<< HEAD
 
 # Framework Location 
 - [Framework Branch](https://github.com/asingh403/API-POM-NOV21/tree/master)
 - [Excel Utility Read and Populated](https://github.com/asingh403/API-POM-NOV21/blob/master/src/main/java/com/qa/api/gorest/util/ExcelUtil.java)
+=======
+>>>>>>> bbf41fa (new project)
---
+++ src/test/java/com/qa/api/gorest/tests/GetImgurAPITest.java
@@ -0,0 +1,38 @@
+package com.qa.api.gorest.tests;
+
+import java.util.Map;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.qa.api.gorest.restclient.RestClient;
+import com.qa.api.gorest.util.Token;
+
+import io.restassured.response.Response;
+
+public class GetImgurAPITest {
+	
+	Map<Object, Object> tokenMap;
+	
+	String accessToken;
+	String accountUserName;
+	String refreshToken;
+	
+	@BeforeMethod
+	public void setUp() {
+		Token.getAccessToken();
+		accessToken = tokenMap.get("access_token").toString();
+		accountUserName = tokenMap.get("account_username").toString();
+		refreshToken = tokenMap.get("refresh_token").toString();
+	}
+	
+	@Test
+	public void accountBlockStatus() {
+		
+		Response response = RestClient.doGet(null, "https://api.imgur.com", "/account/v1/"+accountUserName+"/block", accessToken, null, true);
+		System.out.println(response.statusCode());
+		System.out.println(response.prettyPrint());
+	}
+	
+
+}
